8127e4d99b5222dbde5a3b6326553bab30370b8a
BRoN381 <91597656+BRoN381@users.noreply.github.com>
2022-10-19 14:07:14 +0800

    Update cameratest.py

--- cameratest.py
+++ cameratest.py
@@ -2,21 +2,21 @@ import cv2
 import numpy as np
 
 frontCap = cv2.VideoCapture(0)
-sideCap = cv2.VideoCapture(1)
+# sideCap = cv2.VideoCapture(1)
 
 while True:
 	ret1, frontFrame = frontCap.read()
 	ret2, sideFrame = sideCap.read()
 	frontFrame = cv2.resize(frontFrame, (640, 480))
-	frontFrame = cv2.flip(frontFrame, -1)
-	sideFrame = cv2.resize(sideFrame, (640, 480))
+	# frontFrame = cv2.flip(frontFrame, -1)
+	# sideFrame = cv2.resize(sideFrame, (640, 480))
 	if ret1:
 		cv2.imshow('front', frontFrame)
-		cv2.imshow('side', sideFrame)
+		# cv2.imshow('side', sideFrame)
 	else:
 		print('error')
 	if cv2.waitKey(1)==ord("q"):
 		break
 frontCap.release()
-sideCap.release()
+# sideCap.release()
 cv2.destroyAllWindows()
