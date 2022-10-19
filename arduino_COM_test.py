4e65bb556fe12cd3a05f7c7056bd6d7e6253afd0
BRoN381 <91597656+BRoN381@users.noreply.github.com>
2022-10-19 16:03:43 +0800

    Update arduino_COM_test.py

--- arduino_COM_test.py
+++ arduino_COM_test.py
@@ -2,7 +2,7 @@ import serial
 from time import sleep
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
-
+ser.write('000111'.encode('utf-8'))
 while True:
     while ser.in_waiting:
         serinput = int(ser.readline().decode('utf-8'))
