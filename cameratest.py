e29140024e3aee145cc2b124d2052959502b048a
BRoN381 <91597656+BRoN381@users.noreply.github.com>
2022-10-19 17:05:47 +0800

    Update cameratest.py

--- cameratest.py
+++ cameratest.py
@@ -3,12 +3,16 @@ import numpy as np
 
 frontCap = cv2.VideoCapture(0)
 sideCap = cv2.VideoCapture(1)
+bottomCap = cv2.VideoCapture(2)
 while True:
     ret1, frontFrame = frontCap.read()
     ret2, sideFrame = sideCap.read()
+    ret3, bottomFrame = bottomCap.read()
     if ret1:
         frontFrame = cv2.resize(frontFrame, (640, 480))
         sideFrame = cv2.resize(sideFrame, (640, 480))
+        bottomFrame = cv2.resize(bottomFrame, (640, 480))
+        cv2.imshow('bottom', bottomFrame)
         cv2.imshow('front', frontFrame)
         cv2.imshow('side', sideFrame)
     else:
@@ -17,4 +21,5 @@ while True:
         break
 frontCap.release()
 sideCap.release()
+bottomCap.release()
 cv2.destroyAllWindows()
